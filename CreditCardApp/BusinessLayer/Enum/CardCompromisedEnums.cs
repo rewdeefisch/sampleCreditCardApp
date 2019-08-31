be1db11bce11fd1801556486627b5085f97abc42
rudfischb <44246592+rudfischb@users.noreply.github.com>
2019-08-31 15:37:13 -0400

    Made some progress, g2g

---
+++ CreditCardApp/BusinessLayer/Enum/CardCompromisedEnums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCardApp.BusinessLayer.Enum
+{
+    public enum CardCompromisedEnums
+    {
+        Lost, Stolen, Damaged, None
+    }
+}
---
+++ CreditCardApp/Models/CardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CreditCardApp.BusinessLayer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCardApp.Models
+{
+    public class CardViewModel
+    {
+        public double LastFourDigits { get; set; }
+
+        public string CardHolderName { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public double SecurityCode { get; set; }
+
+        public CardCompromisedEnums CardStatus { get; set; }
+    }
+}
